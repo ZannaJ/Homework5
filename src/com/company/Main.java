f43ffcceafef22b8e2b29fd774dcffa28140d933
ZannaJ <zhanna.jelatanceva@gmail.com>
2021-07-08 21:45:45 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Person natalja = new Person();
+        System.out.println("Natalja is " + natalja.getAge() + " years old" );
+        natalja.setAge(30);
+        System.out.println("Natalja is very " + natalja.getComplexion());
+        natalja.setComplexion("tall");
+        System.out.println("Natalja is a beautiful " + natalja.getGender());
+        natalja.setGender("woman");
+        System.out.println("She is " + natalja.getHeight() + " cm height");
+        natalja.setHeight(170);
+
+        natalja.walk();
+        natalja.talk();
+        natalja.eat();
+        natalja.run(true);
+        
+
+    }
+}
---
+++ src/com/company/Person.java
@@ -0,0 +1,50 @@
+package com.company;
+
+public class Person {
+
+    private String gender = "woman";
+    private int height = 170;
+    private String complexion = "tall";
+    private int age = 30;
+
+    public void walk() {
+        System.out.println("Natalja is walking");
+    }
+    public void talk() {
+        System.out.println("Natalja is talking");
+    }
+    public void eat() {
+        System.out.println("She is eating an ice cream");
+    }
+    public void run(boolean sportsmen) {
+        if (sportsmen) {
+            System.out.println("She is a sportsmen");
+        } else {
+            System.out.println("She is lazy");
+        }
+    }
+    public String getGender() {
+        return this.gender;
+    }
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    public int getHeight() {
+        return this.height;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public String getComplexion() {
+        return this.complexion;
+    }
+    public void setComplexion(String complexion) {
+        this.complexion = complexion;
+    }
+    public int getAge() {
+        return this.age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
